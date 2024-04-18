df2aac422d039555ee246ae10605c3cf7e1b5bac
Dev-Hisaki <muharuichijou@gmail.com>
2024-04-18 21:24:40 +0700

    Add : splash_screen

---
+++ app/src/main/java/com/hisaki/stockwiseapp/SplashScreenActivity.kt
@@ -0,0 +1,21 @@
+package com.hisaki.stockwiseapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val logo = findViewById<ImageView>(R.id.logo_app)
+        logo.alpha = 0f
+        logo.animate().setDuration(2000).alpha(1f).withEndAction{
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+           overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+        }
+    }
+}
