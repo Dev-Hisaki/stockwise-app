5990728265c6a07e9b9d37c62990e7c030954791
Dev-Hisaki <muharuichijou@gmail.com>
2024-04-22 17:32:53 +0700

    Add : navbar (not finished) Change : colors.xml, themes.xml Tema diganti menjadi agar floating action button bisa berada di tengah-tengah bottom navigation bar

--- app/src/main/java/com/hisaki/stockwiseapp/MainActivity.kt
+++ app/src/main/java/com/hisaki/stockwiseapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.hisaki.stockwiseapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.hisaki.stockwiseapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_nav_view)
+        bottomNavigationView.background = null
+        bottomNavigationView.menu.getItem(1).isEnabled = false
     }
 }
