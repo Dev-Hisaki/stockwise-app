957c1316b0b8c80581d4bf440378bfc573a8c33e
Renitha <renithasarima@gmail.com>
2024-05-07 22:39:08 +0700

    add PopUp Stok Barang User & PopUp Stok Kurang Tambah Barang Admin

---
+++ app/src/main/java/com/hisaki/stockwiseapp/Popup_Kurangi_StokBarang_Admin.kt
@@ -0,0 +1,20 @@
+package com.hisaki.stockwiseapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Popup_Kurangi_StokBarang_Admin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_popup_kurangi_stok_barang_admin)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
